01ac7582fca6c637f86c017ba4b5ae26411f76a3
Miguel Alonso <malonso@loopbit.com>
2016-07-09 23:31:26 +0100

    Tests now on same level as the bash script

--- tests/simple_comment_test.fish
+++ tests/simple_comment_test.fish
@@ -1,7 +1,11 @@
 ## setup
 source activate.fish
 
-test_dir="${TMPDIR:=/tmp}/_autoenv_$$"
+if [ -z "$TMPDIR" ]
+    set TMPDIR "/tmp"
+end
+set self_PID %self 
+set test_dir "$TMPDIR/_autoenv_$self_PID"
 
 mkdir -p "$test_dir/a/b"
 echo "echo -a/b-" > "$test_dir/a/b/.env"
@@ -12,11 +16,15 @@ echo "echo -c/d-" > "$test_dir/c/d/.env"
 cd $test_dir
 
 ## test
-( cd a   ) # match=/-a-/ ; match!=/-a/b-/ ; status=0
-( cd a/b ) # match=/-a-\n-a/b-/ ; status=0
-( cd c   ) # match=/$^/ ; status=0
-( cd c/d ) # match!=/-c-/ ; match=/-c/d-/ ; status=0
-( cd e   ) # match!=/^$/ ; status!=0
+cd a # match=/-a-/ ; match!=/-a/b-/ ; status=0
+cd ..
+cd a/b # match=/-a-\n-a/b-/ ; status=0
+cd ../..
+cd c # match=/$^/ ; status=0
+cd ..
+cd c/d # match!=/-c-/ ; match=/-c/d-/ ; status=0
+cd ../..
+cd e # match!=/^$/ ; status!=0
 
 ## teardown
 rm -rf "$test_dir"
